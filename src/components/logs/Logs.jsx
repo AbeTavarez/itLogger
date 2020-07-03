42f54b68b0d261ddb64df76a7f048d8495c7565e
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-03 00:01:00 -0400

    preloader component added

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -6,7 +8,7 @@ const Logs = () => {
 
   useEffect(() => {
     getLogs();
-    //* eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   const getLogs = async () => {
@@ -18,17 +20,17 @@ const Logs = () => {
     setLoading(false);
   };
   if (loading) {
-    return <h4>loading...</h4>;
+    return <Preloader />;
   }
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No logs to show...</p>
       ) : (
-        logs.map((log) => <li>{log.message}</li>)
+        logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
