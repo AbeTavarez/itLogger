7f0ad5be46638a89dd33fc3a414c19009c1f9cbe
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-07 17:41:30 -0400

    added edit log component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -15,6 +18,9 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
         <Logs />
       </div>
     </Fragment>
