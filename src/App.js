62a16b7c4d4839f310350d8a4c7d513d575d22ec
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-08 22:02:01 -0400

    tech modal added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -21,6 +23,8 @@ const App = () => {
         <AddBtn />
         <AddLogModal />
         <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
